5f3e41fa882d791596416027a255bacb41936605
khushant wankhede <wankhedekhushant57@gmail.com>
2026-05-10 16:27:54 +0530

    updated dns servies

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const dns = require('dns');
+
+dns.setServers([
+    '8.8.8.8',
+    '1.1.1.1']
+);
 
 dotenv.config();
 
@@ -28,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
     await connectDB();
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`--->   Server running on port ${PORT}    <---`);
     });
 };
 
