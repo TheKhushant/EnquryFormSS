edd80da180c0ae766d96024b203199d9d0157171
Sankalp Singh <sankalp880@gmail.com>
2026-06-03 18:28:49 +0530

    Added chatbot lead delete route

--- Backend/routes/chatLeadRoutes.js
+++ Backend/routes/chatLeadRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const {
-  saveLead,
-  getLeads,
+    saveLead,
+    getLeads,
+    deleteLead
 } = require("../controllers/chatLeadController");
-// const { saveLead } = require("../controllers/chatLeadController");
 
 router.post("/", saveLead);
 router.get("/", getLeads);
-
+router.delete("/:id", deleteLead);
 
 module.exports = router;
